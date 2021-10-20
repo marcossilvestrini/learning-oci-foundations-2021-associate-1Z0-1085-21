3e4486b5dcf5a97a4fceade41a42a96d3ff6dfeb
Your Name <you@example.com>
2021-10-19 23:06:46 -0300

    feat: Add new features

--- scripts/oci_setup.ps1
+++ scripts/oci_setup.ps1
@@ -4,5 +4,5 @@ Set-ExecutionPolicy RemoteSigned
 #Force PowerShell to use TLS 1.2 for Windows 2012 and Windows 2016:
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-# setup oci 
+# setup oci
 oci setup config
